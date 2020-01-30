9540b013605d720239b66134e88ee87c91fc1477
Michal Cernak <cero@clu.sh>
2020-01-31 00:48:20 +0100

    init smeti

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
+import "./App.css";
+import * as data from "./data/kose";
+
+const trashGreen = new Icon({
+  iconUrl: "/icons8-trash-green.svg",
+  iconSize: [25, 25]
+});
+
+const trashYellow = new Icon({
+  iconUrl: "/icons8-trash-yellow.svg",
+  iconSize: [25, 25]
+});
+const trashOrange = new Icon({
+  iconUrl: "/icons8-trash-orange.svg",
+  iconSize: [25, 25]
+});
+const trashRed = new Icon({
+  iconUrl: "/icons8-trash-red.svg",
+  iconSize: [25, 25]
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!!
-        </a>
-      </header>
-    </div>
+    <Map center={[49.195061, 16.606836]} zoom={10}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {data.kose.map(kos => (
+        <Marker 
+          key={kos.id} 
+          position={[kos.location.lat, kos.location.long]} 
+          icon={
+            kos.fullness < 30 ? trashGreen :
+            (kos.fullness < 50 ? trashYellow :
+            (kos.fullness < 70 ? trashOrange : trashOrange ))}
+        />
+      ))}
+    </Map>
   );
 }
 
